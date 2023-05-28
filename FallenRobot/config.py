b39628f0d2ddd2739790247f4ae491ce59ffda6f
˹Sʜɪᴠᴀᴍ ꭙ Jᴀɪsᴡᴀʟ˼❤️ <111850575+Shivam892483@users.noreply.github.com>
2023-05-28 06:33:24 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -2,23 +2,23 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    API_ID = 28564672
-    API_HASH = "89cf205dd07d743a6a869817e559d503"
+    API_ID = 66
+    API_HASH = "89cf20507d743a6a869817e559d503"
 
     CASH_API_KEY = "5YJWIIKG5AUQ73A7"  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
-    DATABASE_URL = "postgres://tmugvibv:Wo86qFDdNUczEGZuKI7zEeAxTKJLzI17@tiny.db.elephantsql.com/tmugvibv"  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgres://tmugvibv:Wo86qFDdNUczEGZuKI7zEeAxTKJLzItiny.db.elephantsql.com/tmugvibv"  # A sql database url from elephantsql.com
 
     EVENT_LOGS = (-1001605285063)  # Event logs channel to note down important bot level events
 
-    MONGO_DB_URI = "mongodb+srv://Shivam0:shivam098@cluster0.m80uulq.mongodb.net/?retryWrites=true&w=majority"  # Get ths value from cloud.mongodb.com
+    MONGO_DB_URI = "mongodb+srv://Shivam0:shivam09ster0.m80uulq.mongodb.net/?retryWrites=true&w=majority"  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
     START_IMG = "https://te.legra.ph/file/581cbe3c457ff23e8baaa.jpg"
 
     SUPPORT_CHAT = "Earn_without_investment01"  # Your Telegram support group chat username where your users will go and bother you
 
-    TOKEN = "5953802361:AAGBTNtGeOTOTRfWGRW4PqdjuLIiQMcNVIk"  # Get bot token from @BotFather on Telegram
+    TOKEN = "5953802361:AAGBTNtGeOTOGRW4PqdjuLIiQMcNVIk"  # Get bot token from @BotFather on Telegram
 
     TIME_API_KEY = "MRW0DO9TOKQB"  # Get this value from https://timezonedb.com/api
 
