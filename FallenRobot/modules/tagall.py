ac05c8a5f088d01c46f7f3d5793d048ab3daed4b
Sʜɪᴠᴀᴍ ꭙ Jᴀɪsᴡᴀʟ <111850575+Shivam892483@users.noreply.github.com>
2023-07-27 00:38:56 +0530

    Update tagall.py

--- FallenRobot/modules/tagall.py
+++ FallenRobot/modules/tagall.py
@@ -5,7 +5,7 @@ from telethon.errors import UserNotParticipantError
 from telethon.tl.functions.channels import GetParticipantRequest
 from telethon.tl.types import ChannelParticipantAdmin, ChannelParticipantCreator
 
-from MukeshRobot import telethn as client
+from FallenRobot import telethn as client
 
 spam_chats = []
 
